2689612320c6e950b9dc4f8ff389eb65da3729a0
JohnToro-CZAF <nhatanh10102005@gmail.com>
2025-10-02 05:01:37 +0000

    added commit files

--- build_pipeline/RUN_pipeline.sh
+++ build_pipeline/RUN_pipeline.sh
@@ -3,22 +3,34 @@
 DIR="."
 
 # Rip the sequences from the imgt website. HTML may change in the future. 
+which python
 mkdir -p $DIR/IMGT_sequence_files/htmlfiles
 mkdir -p $DIR/IMGT_sequence_files/fastafiles
 python $DIR/RipIMGT.py
 
+echo "FINISHED RIPIMGT"
+
 # Format the alignments and handle imgt oddities to put into a consistent alignment format
 mkdir -p $DIR/curated_alignments
 mkdir -p $DIR/muscle_alignments
+which python
 python $DIR/FormatAlignments.py
+
+echo "FINISHED FORMATALIGNMENTS"
 
 # Build the hmms for each species and chain.
 # --hand option required otherwise it will delete columns that are mainly gaps. We want 128 columns otherwise ARNACI will fall over.
 mkdir -p $DIR/HMMs
+which hmmbuild
+echo "Starting HMMBUILD"
 hmmbuild --hand $DIR/HMMs/ALL.hmm $DIR/curated_alignments/ALL.stockholm
+echo "FINISHED HMMBUILD"
 #hmmbuild --hand $DIR/HMMs/ALL_AND_C.hmm $DIR/curated_alignments/ALL_AND_C.stockholm
 
 # Turn the output HMMs file into a binary form. This is required for hmmscan that is used in ARNACI.
+which hmmpress
+echo "Starting HMMPRESS"
 hmmpress -f $DIR/HMMs/ALL.hmm 
+echo "FINISHED HMMPRESS"
 #hmmpress -f $DIR/HMMs/ALL_AND_C.hmm
 
--- setup.py
+++ setup.py
@@ -1,53 +1,74 @@
 import shutil, os, subprocess
 import site, sys
 from importlib import util
-from distutils.core import setup
+from setuptools import setup
 from setuptools.command.install import install
 
 class CustomInstallCommand(install):
-   def run(self):
-       install.run(self)
-       # Post-installation routine
-       ANARCI_LOC = os.path.join(site.getsitepackages()[0], 'anarci') # site-packages/ folder
-       ANARCI_BIN = sys.executable.split('python')[0] # bin/ folder
+    def run(self):
+        install.run(self)
+        # Post-installation routine
+        ANARCI_LOC = os.path.join(site.getsitepackages()[0], 'anarci') # site-packages/ folder
+        ANARCI_BIN = sys.executable.split('python')[0] # bin/ folder
 
-       shutil.copy('bin/ANARCI', ANARCI_BIN) # copy ANARCI executable
-       print("INFO: ANARCI lives in: ", ANARCI_LOC) 
+        shutil.copy('bin/ANARCI', ANARCI_BIN) # copy ANARCI executable
+        print("INFO: ANARCI lives in: ", ANARCI_LOC) 
 
-       # Build HMMs from IMGT germlines
-       os.chdir("build_pipeline")
-       print('INFO: Downloading germlines from IMGT and building HMMs...')
-       print("INFO: running 'RUN_pipeline.sh', this will take a couple a minutes.")
-       proc = subprocess.Popen(["bash", "RUN_pipeline.sh"], stdout = subprocess.PIPE, stderr = subprocess.PIPE)
-       o, e = proc.communicate()
+        # Build HMMs from IMGT germlines
+        print('INFO: Downloading germlines from IMGT and building HMMs...')
+        print("INFO: running 'RUN_pipeline.sh', this will take a couple a minutes.")
+        env = os.environ.copy()
+        env["PYTHON_SCRIPT"] = sys.executable
+        print("INFO: Running 'RUN_pipeline.sh' with Python script: ", env["PYTHON_SCRIPT"])
 
-       print(o.decode())
-       print(e.decode())
-       
-       # Copy HMMs where ANARCI can find them
-       shutil.copy( "curated_alignments/germlines.py", ANARCI_LOC )
-       os.mkdir(os.path.join(ANARCI_LOC, "dat"))
-       shutil.copytree( "HMMs", os.path.join(ANARCI_LOC, "dat/HMMs/") )
-      
-      # Remove data from HMMs generation
-       try:
-           shutil.rmtree("curated_alignments/")
-           shutil.rmtree("muscle_alignments/")
-           shutil.rmtree("HMMs/")
-           shutil.rmtree("IMGT_sequence_files/")
-       except OSError:
-           pass
+        with open("build_pipeline.log", "w") as logfile:
+            proc = subprocess.Popen(
+                ["bash", "RUN_pipeline.sh"],
+                stdout=logfile,
+                stderr=subprocess.STDOUT,
+                env=env,
+                cwd="build_pipeline",
+            )
+            proc.wait()
+        
+        # # Copy HMMs where ANARCI can find them
+        print("INFO: Copying generated files to ANARCI installation directory...")
+        shutil.copy("build_pipeline/curated_alignments/germlines.py", ANARCI_LOC)
+        
+        # Create dat directory if it doesn't exist
+        dat_dir = os.path.join(ANARCI_LOC, "dat")
+        if not os.path.exists(dat_dir):
+            os.makedirs(dat_dir)
+        
+        # Copy HMMs directory
+        hmm_dest = os.path.join(ANARCI_LOC, "dat", "HMMs")
+        if os.path.exists(hmm_dest):
+            shutil.rmtree(hmm_dest)
+        shutil.copytree("HMMs", hmm_dest)
+        print("INFO: HMM files copied to:", hmm_dest)
+        
+        # Remove temporary data from HMMs generation (optional cleanup)
+        try:
+            shutil.rmtree("curated_alignments/")
+            shutil.rmtree("muscle_alignments/")
+            shutil.rmtree("HMMs/")
+            shutil.rmtree("IMGT_sequence_files/")
+            print("INFO: Cleaned up temporary build files")
+        except OSError as e:
+            print("WARNING: Could not clean up some temporary files:", e)
 
-setup(name='anarci',
-     version='1.3',
-     description='Antibody Numbering and Receptor ClassIfication',
-     author='James Dunbar',
-     author_email='opig@stats.ox.ac.uk',
-     url='http://opig.stats.ox.ac.uk/webapps/ANARCI',
-     packages=['anarci'],
-     package_dir={'anarci': 'lib/python/anarci'},
-     data_files = [ ('bin', ['bin/muscle', 'bin/muscle_macOS', 'bin/ANARCI']) ],
-     include_package_data = True,
-     scripts=['bin/ANARCI'],
-     cmdclass={"install": CustomInstallCommand, }, # Run post-installation routine
-    )
+setup(
+    name='anarci',
+    version='1.3',
+    description='Antibody Numbering and Receptor ClassIfication',
+    author='James Dunbar',
+    author_email='opig@stats.ox.ac.uk',
+    url='http://opig.stats.ox.ac.uk/webapps/ANARCI',
+    packages=['anarci'],
+    package_dir={'anarci': 'lib/python/anarci'},
+    data_files=[('bin', ['bin/muscle', 'bin/muscle_macOS', 'bin/ANARCI'])],
+    include_package_data=True,
+    scripts=['bin/ANARCI'],
+    cmdclass={"install": CustomInstallCommand},
+    zip_safe=False,  # Important: prevents wheel building issues
+)
